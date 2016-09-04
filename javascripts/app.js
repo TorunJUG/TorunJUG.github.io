cc1d30536e877d4a90bb2bd1e804016d85af9a61
Piotr Pradzynski <prondzyn@gmail.com>
2016-09-04 16:02:44 +0200

    Site updated at 2016-09-04 14:02:43 UTC

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,22 +1,43 @@
 (function() {
-  var app = angular.module('TorunJugApp', [], function($interpolateProvider) {
+  var app = angular.module('TorunJugApp', ['angular.filter'], function($interpolateProvider) {
   	$interpolateProvider.startSymbol('[[');
   	$interpolateProvider.endSymbol(']]');
 	});
 
-  app.controller('MainController', function($scope, $http) {
-  	$self = this;
-    $self.speakers = [];
-    $self.randomSpeakers = [];
-    $http.get('/hall-of-fame/speakers.json').success(function(data, status, headers, config) {
-      $self.randomSpeakers = new Array(data[Math.floor(Math.random()*data.length)]);
-    	$self.speakers = data;
+  app.controller('HallOfFameController', function($scope, $http) {
+    $scope.speakers = [];
+    $scope.randomSpeakers = [];
+    $http.get('/hall-of-fame/speakers.json').success(function(data) {
+      $scope.randomSpeakers = new Array(data[Math.floor(Math.random()*data.length)]);
+    	$scope.speakers = data;
     });
-    this.imageUrl = function(img) {
+    $scope.imageUrl = function(img) {
     	return "/images/speakers/" + img;
     };
-    this.meetingUrl = function(number) {
+    $scope.meetingUrl = function(number) {
     	return "/meeting/" + number + "/";
     };
+  });
+
+  app.controller('LeadersController', function($scope, $http) {
+    $scope.leaders = [];
+    $http.get('/leaders/leaders.json').success(function(data) {
+      $scope.leaders = data;
+    });
+    $scope.imageUrl = function(img) {
+      return "/images/leaders/" + img;
+    };
+    $scope.readableStatus = function(status) {
+      switch(status) {
+        case "1_ACTIVE":
+          return "Aktywni";
+        case "2_INACTIVE":
+          return "W stanie spoczynku";
+        case "3_HONORABLE":
+          return "Honorowi";
+        default:
+          return "Nie znam takiego statusu";
+      }
+    }
   });
 })();
